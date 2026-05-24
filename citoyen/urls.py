4c3cd561b1e1bd70aade70412d368c9e09bb154f
TheHarivola <ainaharivola2@gmail.com>
2026-05-24 16:31:27 +0300

    ajout pour imprimer notre TDR

--- citoyen/urls.py
+++ citoyen/urls.py
@@ -5,4 +5,6 @@ app_name = 'citoyen'
 
 urlpatterns = [
     path('recherche/', views.cin_search_view, name='search'),
+    path('tdr/pdf/', views.download_tdr_pdf_view, name='tdr_pdf'),
+    path('cin/pdf/<int:pk>/', views.download_cin_pdf_view, name='download_pdf'),
 ]
